b2978f8fdcf58e24a4ce607fef8fba51e4ca4209
Ammar Shah <ammarshah123@gmail.com>
2023-10-11 21:23:29 +0500

    TOD-1 (WIP): Organizes step definitions into groups of Given, When and Then

--- todoer/features/step_definitions/steps.rb
+++ todoer/features/step_definitions/steps.rb
@@ -1,17 +1,20 @@
+# GIVEN
 Given('I am not logged in') do
   # logout_user
 end
 
+Given('I am logged in') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+# WHEN
 When('I go to the {page} page') do |path|
   visit path
 end
 
+# THEN
 Then('I should be able to see the home page content') do
   expect(page).to have_css("h1.tagline", text: Tagline::DEFAULT_TAGLINE)
-end
-
-Given('I am logged in') do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('I should not be able to see the home page content') do
