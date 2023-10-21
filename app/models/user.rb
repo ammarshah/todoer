60d1f94b397caf2a9bb698aadb1ea98a96414548
Ammar Shah <ammarshah123@gmail.com>
2023-10-21 22:14:41 +0500

    TOD-2 (WIP): Uses Active Job built-in asynchronous queue (without any external queuing backend e.g. Sidekiq) to send Devise emails asynchronously

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  protected
+
+  # Use Active Job to deliver Devise emails in the background
+  # https://github.com/heartcombo/devise#active-job-integration
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
