889d2e1200be2d3adf7ce123a49433285b44328a
Ammar Shah <ammarshah123@gmail.com>
2023-10-21 23:40:36 +0500

    TOD-2 (WIP): Adds necessary env var to run the app in production mode

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,4 +1,6 @@
-Dotenv.require_keys(
+# Keys that are required in all environments
+# and they should be stored in .env file
+common_required_keys = [
   "APP_HOST",
   "APP_PORT",
   "POSTGRES_USERNAME",
@@ -9,4 +11,18 @@ Dotenv.require_keys(
   "SMTP_PASSWORD",
   "DEFAULT_FROM_EMAIL",
   "DEFAULT_REPLY_TO_EMAIL"
-)
+]
+
+# Keys that are required in production environment only
+# and they should be stored in .env.production file
+production_required_keys = [
+  "RAILS_SERVE_STATIC_FILES"
+]
+
+required_keys = [*common_required_keys]
+
+if Rails.env.production?
+  required_keys.concat(production_required_keys)
+end
+
+Dotenv.require_keys(required_keys)
