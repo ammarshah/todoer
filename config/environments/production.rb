cbc5b4cafbc4dcd54c6b36c7352bd2615b3a7c8d
Ammar Shah <ammarshah123@gmail.com>
2023-10-21 22:19:13 +0500

    TOD-2 (WIP): Changes production environment log level from 'info' to 'warn' to prevent sensitive data from being leaked into the logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,10 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  #
+  # One potential security risk is mentioned here:
+  #   https://github.com/heartcombo/devise#password-reset-tokens-and-rails-logs
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
