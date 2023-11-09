b769b44fb38833ccdee0a2b8883d3d79849da65b
Ammar Shah <ammarshah123@gmail.com>
2023-11-09 19:44:11 +0500

    Refactors logout Cucumber step

--- features/support/helpers/authentication_helper.rb
+++ features/support/helpers/authentication_helper.rb
@@ -14,15 +14,9 @@ module AuthenticationHelper
     click_button "Continue"
   end
 
-  # For details, see https://github.com/heartcombo/devise/wiki/How-To:-Test-with-Cucumber#cucumber-testing-for-sign-out
   def logout
-    current_driver = Capybara.current_driver
-
-    begin
-      Capybara.current_driver = :rack_test
-      page.driver.submit :delete, path_for('logout'), {}
-    ensure
-      Capybara.current_driver = current_driver
+    if has_link?(href: path_for('logout'))
+      click_link(href: path_for('logout'))
     end
   end
 
