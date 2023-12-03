6c45b9933166d9e3e6eb8c8f470eb562ad7880c4
Ammar Shah <ammarshah123@gmail.com>
2023-12-04 00:09:21 +0500

    Redesigns header

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static classes = ["active"]
+
+  initialize() {
+    this.activate()
+  }
+
+  activate() {
+    if (window.scrollY >= 40) {
+      this.element.classList.add(this.activeClass)
+    } else {
+      this.element.classList.remove(this.activeClass)
+    }
+  }
+}
